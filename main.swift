fbfa3279a01a96cfc4deaf70179b01b90399553d
Heloisa Mendes <108590744+heloomendess@users.noreply.github.com>
2025-03-12 10:47:47 -0300

    aula2- funcoes

--- main.swift
+++ main.swift
@@ -1,63 +1,56 @@
-/*
- 
- 3- Receba um numero pelo terminal, calcule sua tabuada; Para cada resultado da tabuada, verifique se o resultado for primo
- */
+//
+//  main.swift
+//  aula3
+//
+//  Created by Aluno Mack on 12/03/25.
 
-import Foundation
+
+//1- Escreva uma funcao que receba dois ints, cheque se o primeiro é divisivel pelo segundo e retorne um bool com a resposta
+
 /*
-1- Recebendo um numero pelo terminal, verifique se é par ou impar; Se o numero for par, imprima todos os numeros pares de 0 ao numero e após isso faca o mesmo com os numeros impares
- 
-print("Digite um número: ")
-if let input = readLine(), let number = Int(input) {
-    if number % 2 == 0 {
-        print("O número é par!")
-        for i in 1...number {
-            if i % 2 == 0 {
-                print(i)
-            }
-        }
+ import Foundation
+ func divisao(_ a: Int, _ b: Int) -> Bool {
+    return b != 0 && a % b == 0
+    // b != 0 bool
+    // a % b == 0 fazendo a divisao com base no resto
+}
+
+print("Digite o primeiro número: ")
+if let input = readLine(), let number1 = Int(input) {
+    print("Digite o segundo número: ")
+    if let input = readLine(), let number2 = Int(input) {
+        let resultado = divisao(number1, number2)
+        print("O número é divisivel? \(resultado)")
+    } 
         
-    } else {
-        print("O número é impar!")
-        for i in 1...number {
-            if i % 2 != 0 {
-                print(i)
-            }
-        }
-    }
 }
 */
 
-/* 2- Receba um numero e calcule o seu fatorial; em seguida, verifique se o fatorial obtido é um numero par ou impar. Imprima o resultado do fatorial e, em seguida, informe se o resultado é par ou impar*/
-print("Digite um número: ")
-if let input = readLine(), let number = Int(input) {
-    
-    var value: Int = 1
-    let multiplicacao: Int = 1
-   /* func valorFatorial(fNumber: Int) -> Int {
-        if fNumber == 0 {
-            return 1
-        } else {
-            return fNumber * valorFatorial(fNumber: fNumber-1)
-        }
-     }
-    */
-    for i in 1...number {
-        value=value*i
-        print(value)
-        
-        if multiplicacao == i {
-            if(value % 2 == 0) {
-                print("O fatorial é um número par")
-            } else {
-                print("O fatorial é um número impar")
-            }
-        }
-    }
-    
-    //var output = valorFatorial(fNumber: value)
-    print("Número: \(value)")
-    print("O número fatorial é: \(multiplicacao)")
-    
-   
+
+
+// 2- Escreva uma funcao que recebe um Array e um Int e cheque se o Int está presente no Array. Retorne um Bool com a resposta
+
+/*import Foundation
+
+func recebeNumeros(_ array: [Int], _ numeros: Int) -> Bool {
+    return array.contains(numeros)
 }
+
+let numeros = [1, 2, 3, 4, 5, 6]
+print(recebeNumeros(numeros, 7))
+print(recebeNumeros(numeros, 3))
+*/
+
+
+
+// 3- Escreva uma funcao que recebe um int e retorne o fatorial dele. Limitacao: Nao é permitido o uso de loops(for, while...)
+
+import Foundation
+
+func fatorial(_ numero: Int) -> Int {
+    return numero <= 1 ? 1 : numero * fatorial (numero - 1)
+}
+
+print("O fatorial do numero 4 é \(fatorial(4))")
+print("O fatorial do numero 2 é \(fatorial(2))")
+print("O fatorial do numero 3 é \(fatorial(3))")
