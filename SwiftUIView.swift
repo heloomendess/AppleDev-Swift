3d30e1796e28caba481b2623b79abaea5622fd5a
Heloisa Mendes <108590744+heloomendess@users.noreply.github.com>
2025-03-17 22:56:37 -0300

    Update SwiftUIView.swift

--- SwiftUIView.swift
+++ SwiftUIView.swift
@@ -1,9 +1,10 @@
-//
 //  SwiftUIView.swift
 //  aulaSwiftUI
-//
 //  Created by Aluno Mack on 17/03/25.
-//
+
+/*
+Crie uma nova tela que contenha um número que é incrementado toda vez que um botão for pressionado. Tente deixar o mais parecido que conseguir!
+*/
 
 import SwiftUI
 
